e70f0afc6bc50323d4bd2d3ce83a39c3d94e92f8
Phillip Rousu <philliprousu@gmail.com>
2020-03-16 13:42:49 -0700

    Still fleshing out pagination for component

--- client/components/ReviewsNav.js
+++ client/components/ReviewsNav.js
@@ -5,7 +5,8 @@ class ReviewsNav extends Component {
     super(props)
     this.state = {
       buttons: 0,
-      currentButtons: []
+      currentButtons: [],
+      currentButton: 1
     }
   }
 
@@ -17,13 +18,58 @@ class ReviewsNav extends Component {
     })
   }
 
+  changeForward() {
+    this.setState({
+      currentButton: this.state.currentButton += 1
+    })
+    if (this.state.currentButtons[this.state.currentButton] === this.state.buttons - 4) {
+      let numberOfButtons = Math.floor((this.props.reviews.length - 1) / 7)
+      let value = this.state.currentButtons[2];
+      var newButtons = [
+        value + 1,
+        '...',
+        value + 2,
+        value + 3,
+        numberOfButtons
+      ];
+      this.setState({
+        currentButtons: newButtons
+        //what to do with current button
+      })
+    } else if (this.state.currentButton === 4) {
+      let numberOfButtons = Math.floor((this.props.reviews.length - 1) / 7)
+      let value = this.state.currentButtons[2];
+      var newButtons = [
+        value + 1,
+        value + 2,
+        value + 3,
+        '...',
+        numberOfButtons
+      ];
+      this.setState({
+        currentButtons: newButtons,
+        currentButton: 1
+      })
+    }
+  }
+
 
   render() {
     return (
       <div className="reviewNavDiv">
-        {this.state.currentButtons.length > 0 && <button>Back</button>}
-
-        {!this.state.currentButtons.includes(this.state.buttons) && <button onClick={this.props.currentForward}>Forwards</button>}
+        {!this.state.currentButtons.includes(1) && <button className='forward backward'><ion-icon name="chevron-back-outline"></ion-icon></button>}
+        {this.state.currentButtons.map((num, i) => {
+          if (this.state.currentButton === (i + 1)) {
+           return <div key={i} className='currentButton'>{num}</div>
+          }
+          return (
+            <div key={i} className='reviewsButtons'>{num}</div>
+          )
+        })}
+        {!(this.state.currentButtons[this.state.currentButton] > this.state.buttons - 4) && 
+        <button 
+        className='forward' 
+        onClick={() => {this.changeForward(); this.props.currentForward()}}><ion-icon name="chevron-forward-outline"></ion-icon></button>}
       </div>
     )
   }
