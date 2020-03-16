3b7b671b3bd03de2bdbd22c91de5845d085f89d7
Phillip Rousu <philliprousu@gmail.com>
2020-03-16 10:57:40 -0700

    Experimentig to create logic

--- client/components/ReviewsNav.js
+++ client/components/ReviewsNav.js
@@ -1,12 +1,32 @@
-import React from 'react';
-//import moment from 'moment';
+import React, { Component } from 'react';
 
-const ReviewsNav = () => {
-  return (
-    <div className="reviewNavDiv">
-      
-    </div>
-  )
-};
+class ReviewsNav extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      buttons: 0,
+      currentButtons: []
+    }
+  }
+
+  componentDidMount() {
+    let numberOfButtons = Math.floor((this.props.reviews.length - 1) / 7)
+    this.setState({
+      buttons: numberOfButtons,
+      currentButtons: [1, 2, 3, '...', numberOfButtons]
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="reviewNavDiv">
+        {this.state.currentButtons.length > 0 && <button>Back</button>}
+
+        {!this.state.currentButtons.includes(this.state.buttons) && <button onClick={this.props.currentForward}>Forwards</button>}
+      </div>
+    )
+  }
+}
 
 export default ReviewsNav;
