2ad8a5111645e60eb6f974be0c4397a85cf6004b
Phillip Rousu <philliprousu@gmail.com>
2020-03-16 22:11:34 -0700

    Final functionality implemented

--- client/components/ReviewsNav.js
+++ client/components/ReviewsNav.js
@@ -6,35 +6,49 @@ class ReviewsNav extends Component {
     this.state = {
       buttons: 0,
       currentButtons: [],
-      currentButton: 1
+      currentButton: 1,
+      howManyLeft: this.props.reviews.length,
+      below28: false
     };
   }
 
   componentDidMount() {
-    let numberOfButtons = Math.floor((this.props.reviews.length - 1) / 7);
-    this.setState({
-      buttons: numberOfButtons,
-      currentButtons: [1, 2, 3, 4]
-    });
+    if (this.props.reviews.length > 28) {
+      this.setState({
+        currentButtons: [1, 2, 3, 4]
+      });
+    } else {
+      let numberOfButtons = Math.ceil(this.props.reviews.length / 7);
+      let buttonArray = [];
+      for (let i = 0; i < numberOfButtons; i++) {
+        buttonArray.push(i + 1);
+      }
+      this.setState({
+        currentButtons: buttonArray
+      });
+    }
   }
 
   changeForward() {
     this.setState({
-      currentButton: this.state.currentButton += 1
+      currentButton: this.state.currentButton += 1,
     });
-    if ((this.props.reviews.length - this.props.currentPlace) < 20 ) {
-      let lastReviws = this.props.reviews.slice(this.props.currentPlace);
-      console.log(lastReviws);
+    this.navLogic();
+  }
+
+  navLogic() {
+    let lastReviews = this.props.reviews.slice(this.props.currentPlace);
+    if (lastReviews.length < 28 && this.state.currentButton === 5) {
       let value = this.state.currentButtons[3];
-      let remaining = Math.floor(lastReviws.length / 7);
+      let remaining = Math.ceil(lastReviews.length / 7);
       let newButtons = [];
-      for (let i = 1; i <= remaining; i++) {
+      for (let i = 1; i < remaining; i++) {
         newButtons.push(value + i);
       }
       this.setState({
         currentButtons: newButtons,
         currentButton: 1
-      });
+      }); 
     } else if (this.state.currentButton === 5) {
       let value = this.state.currentButtons[3];
       var newButtons = [
@@ -52,7 +66,8 @@ class ReviewsNav extends Component {
 
   changeBackword() {
     this.setState({
-      currentButton: this.state.currentButton -= 1
+      currentButton: this.state.currentButton -= 1,
+      howManyLeft: this.state.howManyLeft + 7
     });
     if (this.state.currentButton === 0) {
       let value = this.state.currentButtons[0];
@@ -69,29 +84,40 @@ class ReviewsNav extends Component {
     }
   }
 
+  howManyLeft() {
+    this.setState({
+      howManyLeft: this.state.howManyLeft - 7
+    });
+  }
+
   render() {
     return (
-      <div className="reviewNavDiv">
-        {!(this.state.currentButtons[this.state.currentButton - 1] === 1) && <button 
-          className='forward backward'
+      <div className='reviewsComponent'>
+        {!(this.state.currentButtons[this.state.currentButton - 1] === 1) && 
+        <button 
+          className='backward'
           onClick={() => { this.changeBackword(); this.props.currentBackword(); }}>
           <ion-icon name="chevron-back-outline"></ion-icon>
         </button>}
-        {this.state.currentButtons.map((num, i) => {
-          if (this.state.currentButton === (i + 1)) {
-            return <div key={i} className='currentButton'>{num}</div>;
-          }
-          return (
-            <div key={i} className='reviewsButtons'>{num}</div>
-          );
-        })}
-       
-        {!((this.props.reviews.length - this.props.currentPlace) < 7) && <button 
+        <div className="reviewNavDiv">
+          {this.state.currentButtons.map((num, i) => {
+            if (this.state.currentButton === (i + 1)) {
+              return <div key={i} className='currentButton'>{num}</div>;
+            }
+            return (
+              <div key={i} className='reviewsButtons'>{num}</div>
+            );
+          })}
+        </div>
+        {!((this.props.reviews.length - this.props.currentPlace) <= 7) && <button 
           className='forward' 
-          onClick={() => { this.changeForward(); this.props.currentForward(); }}><ion-icon name="chevron-forward-outline"></ion-icon></button>}
+          onClick={() => { this.howManyLeft(); this.changeForward(); this.props.currentForward(); }}><ion-icon name="chevron-forward-outline"></ion-icon></button>}
       </div>
+      
     );
   }
 }
 
 export default ReviewsNav;
+
+
