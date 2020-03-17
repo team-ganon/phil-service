8485e3f73412622ccc8c144b83ddec90afc2d94b
Phillip Rousu <philliprousu@gmail.com>
2020-03-17 11:20:46 -0700

    ESLint

--- client/components/ReviewsHeader.js
+++ client/components/ReviewsHeader.js
@@ -8,24 +8,19 @@ const ReviewsHeader = ({average, total}) => {
       <div className="bottomDivOfHeader">
 
         <div className="containerForStarReviews">
-            <div className='star'>
-              <ion-icon name="star"></ion-icon>
-              <p className='averageTotal'>{average}</p>
-            </div>
-            <p className="total average"><span className='reviewCount'>{total}</span> reviews</p>
-        </div>
-          
+          <div className='star'>
+            <ion-icon name="star"></ion-icon>
+            <p className='averageTotal'>{average}</p>
+          </div>
+          <p className="total average"><span className='reviewCount'>{total}</span> reviews</p>
+        </div>   
         <div className='search'>
           <input type='text' placeholder='Search Reviews'/>
-
-            <ion-icon name="search-outline"></ion-icon>
-  
-
+          <ion-icon name="search-outline"></ion-icon>
         </div>
-        
       </div>
     </div>
-  )
+  );
 };
 
 export default ReviewsHeader;
