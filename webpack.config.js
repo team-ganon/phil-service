5fc7cb3aa9974bd43d359e84326c6b0348ea0a87
Phillip Rousu <philliprousu@gmail.com>
2020-03-17 18:50:30 -0700

    file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,16 @@ module.exports = {
           },
         },
       ]
+    },
+    {
+      test: /\.(svg|png|jpg|gif)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets'
+        }
+      }
     }
     ]
   }
