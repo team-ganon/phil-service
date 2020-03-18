afd10445c0e79b66ac8c4b13fb2ede21ecc980d3
Phillip Rousu <philliprousu@gmail.com>
2020-03-18 13:40:00 -0700

    Grammar Fixes, reArrange App tests out of api test

---
+++ test/App.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import App from '../client/components/App';
+
+describe('App Component', () => {
+
+  it('<App /> currentPlace state should be zero', () => {
+    const wrapper = shallow(<App />);
+    const currentPlaceState = wrapper.state().currentPlace;
+    expect(currentPlaceState).toEqual(0);
+  });
+
+  it('App SnapShot Test', () => {
+    const AppJson = renderer.create(<App />).toJSON();
+    expect(AppJson).toMatchSnapshot();
+  });
+
+
+  
+});
