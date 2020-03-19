9b9c82d2ba96893798ad429dd6e58239f982c038
Phillip Rousu <philliprousu@gmail.com>
2020-03-19 11:55:08 -0700

    Mock global fetch

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,8 +1,5 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-const {Response, Request, Headers, fetch} = require('whatwg-fetch');
 Enzyme.configure({ adapter: new Adapter() });
-global.Response = Response;
-global.Request = Request;
-global.Headers = Headers;
+import fetch from '../__mocks__/api';
 global.fetch = fetch;
