73daff08414fc35be007917c16889c31099d7816
Phillip Rousu <philliprousu@gmail.com>
2020-03-19 11:53:42 -0700

    Mock fetch

--- __mocks__/api.js
+++ __mocks__/api.js
@@ -1,53 +1,54 @@
-const reviewApiCall = () => {
-  return new Promise.resolve({
-    reviews: [{
-      '_id': '5e718b9cce115a5271351577',
-      'user': '5e718b9bce115a5271351513',
-      'rental': 1,
-      'body': 'Cumque pariatur quisquam quibusdam non qui qui ratione. Autem ratione rerum ut id rerum voluptatem adipisci iure accusantium. Ipsum voluptas maiores occaecati quaerat est corporis omnis perspiciatis.',
-      'cleanliness': 4,
-      'communication': 4,
-      'value': 1,
-      'accuracy': 1,
-      'checkIn': 4,
-      'location': 4,
-      'date': '2020-03-18T02:46:52.298Z',
-      '__v': 0,
-      'userProfile': {
-        '_id': '5e718b9bce115a5271351513',
-        'name': 'David Purdy',
-        'imageUrl': 'https://hackreactoramazonfrontendcapstone.s3-us-west-2.amazonaws.com/1.jpeg',
-        '__v': 0
-      }],
-    currentReviews: [{
-      '_id': '5e718b9cce115a5271351577',
-      'user': '5e718b9bce115a5271351513',
-      'rental': 1,
-      'body': 'Cumque pariatur quisquam quibusdam non qui qui ratione. Autem ratione rerum ut id rerum voluptatem adipisci iure accusantium. Ipsum voluptas maiores occaecati quaerat est corporis omnis perspiciatis.',
-      'cleanliness': 4,
-      'communication': 4,
-      'value': 1,
-      'accuracy': 1,
-      'checkIn': 4,
-      'location': 4,
-      'date': '2020-03-18T02:46:52.298Z',
-      '__v': 0,
-      'userProfile': {
-        '_id': '5e718b9bce115a5271351513',
-        'name': 'David Purdy',
-        'imageUrl': 'https://hackreactoramazonfrontendcapstone.s3-us-west-2.amazonaws.com/1.jpeg',
-        '__v': 0
-      }],
-    average: 4,
-    total: 84,
-    cleanliness: 4,
-    communication: 4,
-    value: 3,
-    accuracy: 3,
-    checkIn: 4,
-    location: 4
-  });
+module.exports = {
+  reviewApiCall: () => {
+    return new Promise.resolve({
+      reviews: [{
+        '_id': '5e718b9cce115a5271351577',
+        'user': '5e718b9bce115a5271351513',
+        'rental': 1,
+        'body': 'Cumque pariatur quisquam quibusdam non qui qui ratione. Autem ratione rerum ut id rerum voluptatem adipisci iure accusantium. Ipsum voluptas maiores occaecati quaerat est corporis omnis perspiciatis.',
+        'cleanliness': 4,
+        'communication': 4,
+        'value': 1,
+        'accuracy': 1,
+        'checkIn': 4,
+        'location': 4,
+        'date': '2020-03-18T02:46:52.298Z',
+        '__v': 0,
+        'userProfile': {
+          '_id': '5e718b9bce115a5271351513',
+          'name': 'David Purdy',
+          'imageUrl': 'https://hackreactoramazonfrontendcapstone.s3-us-west-2.amazonaws.com/1.jpeg',
+          '__v': 0
+        }}],
+      currentReviews: [{
+        '_id': '5e718b9cce115a5271351577',
+        'user': '5e718b9bce115a5271351513',
+        'rental': 1,
+        'body': 'Cumque pariatur quisquam quibusdam non qui qui ratione. Autem ratione rerum ut id rerum voluptatem adipisci iure accusantium. Ipsum voluptas maiores occaecati quaerat est corporis omnis perspiciatis.',
+        'cleanliness': 4,
+        'communication': 4,
+        'value': 1,
+        'accuracy': 1,
+        'checkIn': 4,
+        'location': 4,
+        'date': '2020-03-18T02:46:52.298Z',
+        '__v': 0,
+        'userProfile': {
+          '_id': '5e718b9bce115a5271351513',
+          'name': 'David Purdy',
+          'imageUrl': 'https://hackreactoramazonfrontendcapstone.s3-us-west-2.amazonaws.com/1.jpeg',
+          '__v': 0
+        }}],
+      average: 4,
+      total: 84,
+      cleanliness: 4,
+      communication: 4,
+      value: 3,
+      accuracy: 3,
+      checkIn: 4,
+      location: 4
+    });
+  }
 };
-
-exports.reviewApiCall = reviewApiCall;
+ 
 
